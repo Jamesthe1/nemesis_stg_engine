d04acc42d61c2d50454e06d81cd4418a187cf8fc
Jamesthe1 <jamesthe1sky@gmail.com>
2024-02-09 15:58:17 -0600

    Add trigger functionality, change trigger event params

--- StageTrigger.cs
+++ StageTrigger.cs
@@ -20,6 +20,49 @@ public partial class StageTrigger : Marker2D {
     [Export]
     public NodePath jump;
 
+    [Export]
+    public bool fireOnce = true;
+
+    public bool disabled;
+
+    public override void _Ready () {
+        string visCheckName = "VisCheck";
+        if (!HasNode (visCheckName)) {
+            var visCheck = this.CreateChild<VisibleOnScreenNotifier2D> (visCheckName);
+            visCheck.ScreenEntered += ScreenTriggerCheck;
+        }
+    }
+
+    public override void _ExitTree () {
+        GetNode<VisibleOnScreenNotifier2D> ("VisCheck").ScreenEntered -= ScreenTriggerCheck;
+    }
+
+    public override void _PhysicsProcess (double delta) {
+        bool Passed (int axis) {
+            Vector2 stage = -STGController.Instance.Position;
+            Vector2 moveDir = STGController.Instance.stageMovement.Sign ();
+            if (moveDir[axis] == 1)
+                return stage[axis] >= Position[axis];
+            return stage[axis] <= Position[axis];
+        }
+
+        if (condition == TriggerCondition.PassX && Passed (0) ||
+            condition == TriggerCondition.PassY && Passed (1))
+            FireTrigger ();
+    }
+
+    public void ScreenTriggerCheck () {
+        if (condition == TriggerCondition.OnSeen)
+            FireTrigger ();
+    }
+
+    private void FireTrigger () {
+        if (!disabled)
+            EmitSignal ("Trigger", this);
+        if (fireOnce)
+            disabled = true;
+    }
+
     [Signal]
-    public delegate void TriggerEventHandler (TriggerType type);
+    public delegate void TriggerEventHandler (StageTrigger triggerNode);
 }
